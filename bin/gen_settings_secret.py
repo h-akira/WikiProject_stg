16b178609336e3cb186dfa2fd86e7041f89401a7
h-akira <79441679+h-akira@users.noreply.github.com>
2024-12-31 00:50:03 +0900

    fix

--- bin/gen_settings_secret.py
+++ bin/gen_settings_secret.py
@@ -36,6 +36,7 @@ SOURCE = TEMPLATE.format(
   region=os.getenv("settings_secret_region"),
   userPoolID=os.getenv("settings_secret_userPoolID"),
   clientID=os.getenv("settings_secret_clientID"),
+  ACM_ARN=os.getenv("settings_secret_ACM_ARN"),
   MAPPING_PATH=MAPPING_PATH,
   DEBUG=os.getenv("settings_secret_DEBUG")
 )
